cb082ec587d7a07bf68fd6fa596d78b5c99d265f
JavierGH2025 <javier.marzo2020@gmail.com>
2025-06-09 20:03:03 +0200

    Deploy

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,13 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('APP_URL', 'https://nutrifit.up.railway.app'),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
   },
 });
